a6337c517e8bb4a247323c9b614dc9edd4f7b8bd
Ryan Kim 김현호 <ryankim.h.dev@gmail.com>
2022-10-24 15:03:52 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,20 @@
+function solution(denum1, num1, denum2, num2) {
+    const res = [];
+    // num: 분모
+    const num = num1 * num2;
+    // denum: 분자
+    const denum = denum1 * num2 + denum2 * num1;
+    
+ 
+    // 분자, 분모 둘 다 둘 중 작은수로 나눈 나머지가 0이어야 함.
+    let minNumber = Math.max(num, denum);
+    
+    while (true) {
+            if (denum % minNumber === 0 && num % minNumber === 0) {
+        return [denum / minNumber, num / minNumber];
+    }
+    minNumber = minNumber - 1;
+    }
+    
+
+} 
