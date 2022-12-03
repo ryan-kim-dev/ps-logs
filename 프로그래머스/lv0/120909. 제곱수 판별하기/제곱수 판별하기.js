4f21d9ef26161b1153bf80c57d362e1436542b89
Ryan Kim 김현호 <ryankim.h.dev@gmail.com>
2022-12-03 22:32:38 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,6 +1,10 @@
 function solution(n) {
-   for (let i = 1; i <= 1000000; i++) {
-       if (i * i === n) return 1; 
-   }
-    return 2;
+   // for (let i = 1; i <= 1000000; i++) {
+   //     if (i * i === n) return 1; 
+   // }
+   //  return 2;
+    const answer = Math.sqrt(n) === Math.round(Math.sqrt(n)) ?
+          1 : 2;
+    return answer;
+        
 }
