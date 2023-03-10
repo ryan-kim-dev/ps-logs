53392bb90064041dd9ad87650e385e654d7d4b07
Ryan Kim 김현호 <ryankim.h.dev@gmail.com>
2023-03-10 10:55:50 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+    let answer = [];
+    for (let i = 0; i < my_string.length; i++) {
+        if (!isNaN(my_string[i])) answer.push(Number(my_string[i]));
+    }
+    answer.sort((a, b) => a - b);
+    return answer;
+}
