b11f5b1fe9912fbfdf3aa355a6fae7cd3709a9df
Ryan Kim 김현호 <ryankim.h.dev@gmail.com>
2024-03-16 18:08:26 +0900

    Time: 67 ms (5.18%), Space: 48.4 MB (82.08%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    
+    function toBe(n) {
+        if (n === val) return true;
+        
+        throw new Error("Not Equal");
+    }
+
+    function notToBe(n) {
+        if (val !== n) return true;
+
+        throw new Error('Equal');
+    }
+
+    return { toBe, notToBe };
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
