9d2cf6851257dffba06b51a3ce33a185072cde1f
Ryan Kim 김현호 <ryankim.h.dev@gmail.com>
2024-03-16 18:12:49 +0900

    Time: 57 ms (68.19%), Space: 52.2 MB (8.37%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let val = init;
+    function increment() {
+        val += 1;
+        return val;
+    }
+    function decrement() {
+        val -= 1;
+        return val;
+    }
+    function reset() {
+        val = init;
+        return val;
+    }
+
+    return { increment, decrement, reset };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
